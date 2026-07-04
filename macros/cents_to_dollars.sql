136cb5982d73f8b295c95cb094d13aa470222cc4
Sukriti Sachan <sukritisachan02@gmail.com>
2026-07-04 09:58:30 +0000

    DeletingDarshil'sFilesFromjayzernBranch

--- macros/cents_to_dollars.sql
+++
@@ -1,3 +0,0 @@
-{% macro cents_to_dollars(column_name) %}
-    round({{ column_name }} / 100, 2)
-{% endmacro %}
--- tests/assert_payment_is_positive.sql
+++
@@ -1,6 +0,0 @@
-select
-    payment_id,
-    amount
-from {{ ref('stg_payments') }}
-where payment_status = 'success'
-  and amount <= 0
